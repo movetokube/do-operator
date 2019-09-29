0f79e7094517c00c9b4ef4a2b6729f89be965dc1
Tomas <tomas@adomavicius.com>
2019-09-29 20:55:58 +0300

    missing modules

--- vendor/github.com/grpc-ecosystem/grpc-gateway/runtime/proto_errors.go
+++ vendor/github.com/grpc-ecosystem/grpc-gateway/runtime/proto_errors.go
@@ -35,7 +35,7 @@ func DefaultHTTPProtoErrorHandler(ctx context.Context, mux *ServeMux, marshaler 
 
 	contentType := marshaler.ContentType()
 	// Check marshaler on run time in order to keep backwards compatability
-	// An interface param needs to be added to the ContentType() function on
+	// An interface param needs to be added to the ContentType() function on 
 	// the Marshal interface to be able to remove this check
 	if httpBodyMarshaler, ok := marshaler.(*HTTPBodyMarshaler); ok {
 		pb := s.Proto()
